d203dd27456caf025525102edfe1e43a9297af08
Katteu <katheamari@gmail.com>
2023-03-13 19:38:50 +0800

    Updated code

--- G6_TicTacToe/G6_TicTacToe/MainPage.xaml.cs
+++ G6_TicTacToe/G6_TicTacToe/MainPage.xaml.cs
@@ -10,9 +10,127 @@ namespace G6_TicTacToe
 {
     public partial class MainPage : ContentPage
     {
+        int turn=1,count=0;
         public MainPage()
         {
             InitializeComponent();
+            logo.Source = ImageSource.FromResource("G6_TicTacToe.Images.tictactoelogo.png");
+        }
+
+        private void btnClicked(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            if (turn == 1)
+            {
+                btn.Text = "X";
+                turn = 0;
+                LabelO.Text = "O Player's turn";
+                LabelX.Text = "X Player waits";
+            }
+            else
+            {
+                btn.Text = "O";
+                turn = 1;
+                LabelX.Text = "X Player's turn";
+                LabelO.Text = "O Player waits";
+            }
+            btn.IsEnabled = false;
+            validate(btn.Text);
+        }
+
+        private void validate(string btnContent)
+        {
+            if ((buttonOne.Text == btnContent && buttonTwo.Text == btnContent && buttonThree.Text == btnContent) ||
+                (buttonOne.Text == btnContent && buttonFive.Text == btnContent && buttonNine.Text==btnContent)||
+                (buttonOne.Text == btnContent && buttonFour.Text ==btnContent && buttonSeven.Text==btnContent)||
+                (buttonTwo.Text == btnContent && buttonFive.Text ==btnContent && buttonEight.Text ==btnContent)||
+                (buttonThree.Text==btnContent && buttonSix.Text ==btnContent && buttonNine.Text ==btnContent)||
+                (buttonThree.Text==btnContent && buttonFive.Text==btnContent && buttonSeven.Text==btnContent)||
+                (buttonFour.Text==btnContent && buttonFive.Text == btnContent && buttonSix.Text==btnContent)||
+                (buttonSeven.Text==btnContent && buttonEight.Text == btnContent && buttonNine.Text==btnContent))
+            {
+                switch (btnContent)
+                {
+                    case "O":
+                        LabelO.Text = "O Player won!";
+                        LabelX.Text = "X Player lost :(";
+                        break;
+                    case "X":
+                        LabelO.Text = "O Player lost :(";
+                        LabelX.Text = "X Player won!";
+                        break;
+                    default:
+                        break;
+                }
+                disableBtn();
+            }
+            else
+            {
+                count = 0;
+                foreach (Button btn in parentElem1.Children)
+                {
+                    if (btn.IsEnabled == false) { count++; }
+                }
+                foreach (Button btn in parentElem2.Children)
+                {
+                    if (btn.IsEnabled == false) { count++; }
+                }
+                foreach (Button btn in parentElem3.Children)
+                {
+                    if (btn.IsEnabled == false) { count++; }
+                }
+
+                if (count >= 9)
+                {
+                    LabelX.Text = "It is a tie!";
+                    LabelO.Text = "No one wins!";
+                }
+            }
+        }
+
+        private void disableBtn()
+        {
+            foreach (Button btn in parentElem1.Children)
+            {
+                btn.IsEnabled = false;
+            }
+            foreach (Button btn in parentElem2.Children)
+            {
+                btn.IsEnabled = false;
+            }
+            foreach (Button btn in parentElem3.Children)
+            {
+                btn.IsEnabled = false;
+            }
+            resetBtn.IsEnabled = true;
+        }
+
+        private void resetClicked(object sender, EventArgs e)
+        {
+            foreach(Button btn in parentElem1.Children)
+            {
+                btn.Text = "";
+                btn.IsEnabled = true;
+                LabelX.Text = "X Player's turn";
+                LabelO.Text = "O Player waits";
+                turn = 1;
+            }
+            foreach (Button btn in parentElem2.Children)
+            {
+                btn.Text = "";
+                btn.IsEnabled = true;
+                LabelX.Text = "X Player's turn";
+                LabelO.Text = "O Player waits";
+                turn = 1;
+            }
+            foreach (Button btn in parentElem3.Children)
+            {
+                btn.Text = "";
+                btn.IsEnabled = true;
+                LabelX.Text = "X Player's turn";
+                LabelO.Text = "O Player waits";
+                turn = 1;
+            }
         }
     }
 }
